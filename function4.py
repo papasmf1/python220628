ee6f6aad9c0d3918405610a7f61cb54204afd163
papasmf1 <papasmf1@gmail.com>
2022-06-28 14:51:21 +0900

    함수 연습 완료

--- function4.py
+++ function4.py
@@ -30,3 +30,25 @@ def union(*ar):
 #호출
 print(union("HAM","SPAM"))
 print(union("HAM","SPAM","EGG"))
+
+#정의되지 않은 인자 처리
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234"))
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234", 
+    name="mike"))
+
+print(globals())
+
+#람다함수 정의
+g = lambda x,y:x*y 
+print(g(3,4))
+print(g(5,6))
+print( (lambda x:x*x)(3) )
+
+print(globals())
