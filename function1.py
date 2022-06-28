e667c64a2cc84dbfa0a18be80101683b50d4a3c9
papasmf1 <papasmf1@gmail.com>
2022-06-28 11:34:27 +0900

    함수 연습1

---
+++ function1.py
@@ -0,0 +1,16 @@
+# function1.py 
+#함수를 정의
+def times(a,b):
+    return a*b 
+
+#함수를 호출
+print(times(3,4))
+
+#함수 정의
+def setValue(x):
+    print("값:", x)
+
+#함수 호출
+result = setValue(5)
+print(result)
+
