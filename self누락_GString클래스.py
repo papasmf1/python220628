76eec08e151cf9a621593ae2db47dd960e4abc07
papasmf1 <papasmf1@gmail.com>
2022-06-29 10:36:49 +0900

    클래스 연습3

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,17 @@
+#전역변수 
 str = "Not Class Member"
+
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수 
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #파이썬은 명확하게 코딩하는 것이 좋다! 
+        print(self.str)
 
+#인스턴스(복사본)
 g = GString()
 g.set("First Message")
 g.print()
--- 생성자소멸자_참조카운트.py
+++ 생성자소멸자_참조카운트.py
@@ -1,16 +1,19 @@
 # -*- 생성자와 소멸자 그리고 참조 카운트 관리  -*-
 class MyClass:
+    #생성자 
     def __init__(self, value):
         self.value = value
         print("Instance is created! Value = ", value)
+    #소멸자 
     def __del__(self):
         print("Instance is deleted!")
 
 #인스턴스 생성 
 d = MyClass(10)
-d_copy = d
-del d_copy 
-del d
+#del d 
+
+#주기적으로 가비지 컬렉션(GC)서비스가 동작된다. 
+print("---전체 코드 실행 종료---")
 
 
 
