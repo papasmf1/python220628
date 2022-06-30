1bae32a38183d677b9595fb2ff9ae88ae7a5abcf
papasmf1 <papasmf1@gmail.com>
2022-06-30 14:34:30 +0900

    네이버 웹툰 크롤링 2

--- web3.py
+++ web3.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 #웹서버에 요청
 import urllib.request
 
+#파일로 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
 #페이징 처리를 하는 경우
 for i in range(1,6):
     url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
@@ -11,18 +13,13 @@ for i in range(1,6):
     data = urllib.request.urlopen(url)
     #검색이 용이한 객체
     soup = BeautifulSoup(data, "html.parser")
-    # <td class="title">
-    # 	<a href="/webtoon/detail">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
-    # </td>
+
     cartoons = soup.find_all("td", class_="title")
-    #리스트형태 객체:0, 1번
-    title = cartoons[0].find("a").text 
-    link = cartoons[0].find("a")["href"]
-    print("갯수:{0}".format(len(cartoons)))
-    print(title)
-    print(link)
 
     for item in cartoons:
         title = item.find("a").text 
         print(title.strip())
+        f.write(title.strip() + "\n")
 
+f.close() 
+print("---웹 크롤링 종료---")
