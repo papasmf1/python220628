51a2238387b2cfba2224854dfaa096cfa0abadc2
papasmf1 <papasmf1@gmail.com>
2022-06-30 15:40:38 +0900

    오늘의 유머 크롤링

--- 판오늘의유머_베스트게시.py
+++ 판오늘의유머_베스트게시.py
@@ -8,10 +8,10 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 #파일로 저장
-f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+f = open("c:\\work\\todayHumor.txt", "wt", encoding="utf-8")
+for n in range(1,11):
+        #오늘의 유머 게시판
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -21,16 +21,15 @@ for n in range(0,10):
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
 
-        # <span class="subject_fixed" data-role="list-title-text" title="아이패드 프로 11인치 2세대 (2020) 셀룰러 256기가 스그">
-        # 							아이패드 프로 11인치 2세대 (2020) 셀룰러 256기가 스그
-        # 						</span>
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        #<td class="subject">
+        # <a href="/board/view.php?table=bestofbest&no=456782">미국 백수의 삶</a><
+        list = soup.find_all('td', attrs={'class':'subject'})
 
         for item in list:
                 try:
-                        title = item.text
+                        title = item.find("a").text
                         #print(title.strip())
-                        if (re.search('맥북', title)):
+                        if (re.search('누리호', title)):
                                 print(title.strip())
                                 f.write(title.strip() + "\n")
 
