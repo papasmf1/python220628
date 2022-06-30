be785f36245684bdec95fe4ad193e85bced2cda5
papasmf1 <papasmf1@gmail.com>
2022-06-30 10:51:47 +0900

    정규표현식 연습2

--- DemoRE.py
+++ DemoRE.py
@@ -1,10 +1,16 @@
 # DemoRE.py 
 import re 
 
-result = re.search("[0-9]*th", "35th")
+result = re.search("[0-9]*th", "  35th")
+#매칭 오브젝트가 리턴 
 print(result)
 print(result.group())
 
-result = re.match("[0-9]*th", "35th")
-print(result)
-print(result.group())
+#블럭 주석 처리: ctrl + / 
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+print(bool(re.search("apple", "this is apple")))
+print(bool(re.match("apple", "this is apple")))
+
