2fe2017b8a8be38b0b0a62dd8cde81dc74f60d90
papasmf1 <papasmf1@gmail.com>
2022-06-30 13:47:39 +0900

    웹 크롤링 연습2

--- web1.py
+++ web1.py
@@ -5,5 +5,23 @@ from bs4 import BeautifulSoup
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+#print(soup.prettify())
+#<p>태그 몽땅 검색 
+#print(soup.find_all("p"))
+#첫번째<p>태그 검색
+#print(soup.find("p"))
+
+#조건이 있는 경우: <p class='outer-text'>
+#print(soup.find_all("p", class_="outer-text"))
+
+#id속성으로 검색
+#print(soup.find_all(id="first"))
+
+#태그 안쪽의 문자열 데이터
+for item in soup.find_all("p"):
+    #내부 문자열: .text 
+    title = item.text 
+    title = title.replace("\n", "")
+    title = title.replace("\t", "")
+    print(title.strip())
 
